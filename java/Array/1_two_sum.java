dfdaadbc7e315d5871a1ab6b6209bf9bbadc81af
zhuhonglinX <zhuhl1996@gmail.com>
2019-03-10 16:49:11 +0800

    adjust

---
+++ java/Array/1_two_sum.java
@@ -0,0 +1,11 @@
+public static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int tmp = target - nums[i];
+        if (map.containsKey(tmp)) {
+            return new int[]{map.get(tmp), i};
+        }
+        map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("no solution");
+}
