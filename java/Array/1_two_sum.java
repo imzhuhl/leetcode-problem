a4d00db298ff8efd4bf2052a23c33d706f36457f
zhuhonglinX <zhuhl1996@gmail.com>
2019-03-10 17:12:10 +0800

    array 1

--- java/Array/1_two_sum.java
+++ java/Array/1_two_sum.java
@@ -1,3 +1,12 @@
+/** 
+    use hash table
+    time complexity O(n), space complexity O(n)
+
+    maybe another solution:
+        1. sort the nums
+        2. two pointers
+ */
+
 public static int[] twoSum(int[] nums, int target) {
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i < nums.length; i++) {
