120fc907e344e4849cb55f32fc47ec1477e02204
zhuhonglinX <zhuhl1996@gmail.com>
2019-04-18 21:35:39 +0800

    Array 11 16

--- java/Array/11_Container_With_Most_Water.java
+++ java/Array/11_Container_With_Most_Water.java
@@ -4,19 +4,19 @@
  */
 
 public static int maxArea(int[] height) {
-        int left = 0, right = height.length - 1;
-        int maxS = 0;
-        while (left < right) {
-            int h, tmp = (right - left);
-            if (height[left] < height[right]) {
-                h = height[left];
-                left++;
-            } else {
-                h = height[right];
-                right--;
-            }
-            tmp = tmp * h;
-            if (maxS < tmp) maxS = tmp;
+    int left = 0, right = height.length - 1;
+    int maxS = 0;
+    while (left < right) {
+        int h, tmp = (right - left);
+        if (height[left] < height[right]) {
+            h = height[left];
+            left++;
+        } else {
+            h = height[right];
+            right--;
         }
-        return maxS;
+        tmp = tmp * h;
+        if (maxS < tmp) maxS = tmp;
     }
+    return maxS;
+}
