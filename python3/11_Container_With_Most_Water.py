f5e9d758b8e360f1d1895520daff33f2335e3222
zhuhonglinX <zhuhl1996@gmail.com>
2019-05-25 16:28:23 +0800

    11&7

---
+++ python3/11_Container_With_Most_Water.py
@@ -0,0 +1,20 @@
+from typing import List
+import pdb
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        area = 0
+        while left < right:
+            if height[left] >= height[right]:
+                area = max(area, (right - left) * height[right])
+                right -= 1
+            else:
+                area = max(area, (right - left) * height[left])
+                left += 1
+        return area
+            
+
+
+if __name__ == "__main__":
+    lst = [1, 1]
+    print(Solution().maxArea(lst))
